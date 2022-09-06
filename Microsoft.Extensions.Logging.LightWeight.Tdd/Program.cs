fc67e7f66f231227a6981104fd0630337bcc62f1
Mani Moayedi <acidmanic.moayedi@gmail.com>
2022-09-06 11:41:03 +0430

    Initialize Project

---
+++ Microsoft.Extensions.Logging.LightWeight.Tdd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Extensions.Logging.LightWeight.Tdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
